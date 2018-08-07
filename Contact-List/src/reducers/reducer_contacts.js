a3cff6379e5bd3b7107fbee0a77d673b2ef43516
nirajsingh002 <nirajsingh002@gmail.com>
2018-08-08 00:16:25 +0530

    contact list example

---
+++ Contact-List/src/reducers/reducer_contacts.js
@@ -0,0 +1,15 @@
+export default function() {
+  return [{
+    "name": "Miguel",
+    "phone": "1"
+  }, {
+    "name": "Peter",
+    "phone": "2",
+  }, {
+    "name": "Jessica",
+    "phone": "3",
+  }, {
+    "name": "Michael",
+    "phone": "4"
+  }];
+}
